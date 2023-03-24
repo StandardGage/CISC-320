014a91ca359a3f8048fe33e71ae9c87e3b395a64
StandardGage <gagedolph@yahoo.com>
2023-03-23 21:21:52 -0400

    fixed and documented

--- crack_hash.py
+++ crack_hash.py
@@ -3,6 +3,8 @@ from itertools import permutations
 
 from crack_tools.password_hash import simple_hash
 
+# AdaIsGoodDog
+
 
 def parse_dictionary(file: str) -> List[str]:
     """Parse the dictionary file and fill an array with each word"""
@@ -14,21 +16,17 @@ def parse_dictionary(file: str) -> List[str]:
 def hash_words(words: str, hash_code: int, depth: int = 1):
     """Go through all the permutations for words at a length/depth specified
     to check if the hashed word is the password."""
-    count = 0
     for depth_iteration in range(1, depth+1):
         for permutation in permutations(words, depth_iteration):
-            count += 1
             hash_word = ''.join(permutation)
             hash = simple_hash(hash_word, hash_code)
-            # if(hash == 0):
-            # return hash_word
-    return count
+            if(hash == 0):
+                return hash_word
 
 
 def main():
     HASH_CODE = 81445731
     words = parse_dictionary('crack_tools\dictionary.txt')
-    print(simple_hash("AdaIsGoodDog", HASH_CODE))
     answer = hash_words(words, HASH_CODE, 4)
     print(answer)
 
